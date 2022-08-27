66d669a347cd0840abf0916e885741c3728149b8
JR-Camacho <josecamachoc0303@gmailcom>
2022-08-27 14:11:20 -0400

    Se cambian las rutas de la api para el archivo api.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,4 @@
 <?php
-
-use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +12,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('/posts', PostsController::class);
